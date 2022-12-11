05a5cbf683eafe383a46f59be35e11c3b430f271
Andrew James <contact@ajames.dev>
2022-12-11 18:51:13 +0000

    create basic pages

---
+++ pages/blog.tsx
@@ -0,0 +1,9 @@
+import Layout from "../components/Layout/Layout";
+
+export default function Blog() {
+  return (
+    <Layout>
+      <h1>BLOG</h1>
+    </Layout>
+  );
+}
---
+++ pages/cv.tsx
@@ -0,0 +1,9 @@
+import Layout from "../components/Layout/Layout";
+
+export default function CV() {
+  return (
+    <Layout>
+      <h1>CV</h1>
+    </Layout>
+  );
+}
