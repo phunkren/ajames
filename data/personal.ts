cf36c0dc6106dfecb160dea1426aa3d750a8e929
Andrew James <contact@ajames.dev>
2022-12-17 14:31:43 +0000

    Add personal data

---
+++ data/personal.ts
@@ -0,0 +1,10 @@
+export const PERSONAL = {
+  name: "Andrew James",
+  initials: "AJ",
+  location: "Glasgow, Scotland",
+  occupation: {
+    title: "Frontend Engineer",
+    employer: "Coinbase",
+    url: "https://www.coinbase.com/",
+  },
+};
