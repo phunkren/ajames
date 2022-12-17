b722e6bf39c3e9b09bc25c054cd51d99d175c1b3
Andrew James <contact@ajames.dev>
2022-12-17 14:31:49 +0000

    Add social data

---
+++ data/social.ts
@@ -0,0 +1,24 @@
+import { Social } from "../types/social";
+
+export const SOCIAL: Record<string, Social> = {
+  github: {
+    id: "github",
+    displayName: "GitHub",
+    url: "https://github.com/phunkren",
+  },
+  linkedin: {
+    id: "linkedin",
+    displayName: "LinkedIn",
+    url: "https://www.linkedin.com/in/ajamesdev/",
+  },
+  youtube: {
+    id: "youtube",
+    displayName: "YouTube",
+    url: "https://www.youtube.com/@phunkren",
+  },
+  twitter: {
+    id: "twitter",
+    displayName: "Twitter",
+    url: "https://twitter.com/phunkren",
+  },
+};
