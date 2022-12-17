ac1eab37a902f1116dabd17ba318cb5abe7067d7
Andrew James <contact@ajames.dev>
2022-12-17 12:51:09 +0000

    create transparent button

--- components/Button.tsx
+++ components/Button.tsx
@@ -1,10 +1,24 @@
 import { ButtonHTMLAttributes } from "react";
+import { styled } from "../stitches.config";
 
 type ButtonProps = ButtonHTMLAttributes<HTMLButtonElement> & {
   variant?: "primary" | "secondary";
   transparent?: boolean;
 };
 
+const StyledButton = styled("button", {
+  appearance: "none",
+  background: "none",
+  border: "none",
+  color: "inherit",
+  font: "inherit",
+  overflow: "visible",
+  padding: "0",
+  "line-height": "normal",
+  "-webkit-appearance": "none",
+  "-moz-appearance": "none",
+});
+
 export function Button(props: ButtonProps) {
-  return <button type="button" {...props} />;
+  return <StyledButton type="button" {...props} />;
 }
