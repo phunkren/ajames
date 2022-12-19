c1863f608739bb6007e9018b05211a31b9aedf4f
Andrew James <contact@ajames.dev>
2022-12-19 09:38:03 +0000

    useLocalStorage initialValue optional

--- hooks/useLocalStorage.ts
+++ hooks/useLocalStorage.ts
@@ -2,7 +2,7 @@ import { useState, useEffect } from "react";
 
 export function useLocalStorage<T>(
   key: string,
-  initialValue: T
+  initialValue?: T
 ): [T, (value: T) => void] {
   // State to store our value
   // Pass initial state function to useState so logic is only executed once
