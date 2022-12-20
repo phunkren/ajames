5cf08094ae50896b1e7df308edd30575c308f834
Andrew James <contact@ajames.dev>
2022-12-20 11:30:36 +0000

    added content to employment data

--- data/employment.ts
+++ data/employment.ts
@@ -6,6 +6,9 @@ type Employer = {
   startDate: string;
   endDate: string;
   location: string;
+  content1: string;
+  content2?: string;
+  content3?: string;
 };
 
 export const EMPLOYMENT = [
@@ -17,6 +20,10 @@ export const EMPLOYMENT = [
     startDate: "MMMM-YY",
     endDate: "MMMM-YY",
     location: "City, Country",
+    content1:
+      "In gravida ligula ass fizzle. Prizzle ghetto. Etizzle for sure. Donizzle in mi a erizzle imperdiet vehicula. Ass vizzle go to hizzle. Sizzle est the bizzle lacizzle. Praesent a ipsizzle gangster urna euismizzle fo shizzle mah nizzle fo rizzle, mah home g-dizzle. Integizzle nec fo shizzle. Fo shizzle tellivizzle dang vel da bomb. Morbi my shizz justo. Etiam eu dui izzle magna elementizzle lobortis.",
+    content2:
+      "Donizzle cool auctor maurizzle. Izzle bizzle uhuh ... yih! fizzle nibh pretizzle sheezy. Maecenizzle a sheezy. Shiznit izzle lacus sizzle gangster elementizzle tristique. Nunc check out this tortizzle sit amet pot ultricizzle break yo neck, yall. In mah nizzle yo, we gonna chung izzle, quizzle, adipiscing quis, pimpin. Etizzle velit leo, aliquam shiznit, pharetra nizzle, go to hizzle sizzle, get down get down. Vivamus gangster.",
   },
   {
     id: "employer2",
@@ -26,6 +33,8 @@ export const EMPLOYMENT = [
     startDate: "MMMM-YY",
     endDate: "MMMM-YY",
     location: "City, Country",
+    content1:
+      "Crizzle break it down justo i saw beyonces tizzles and my pizzle went crizzle purizzle break it down ornare. Dawg venenatis justo izzle daahng dawg. Shizznit fizzle. Suspendisse fizzle placerat hizzle. Dang own yo' ante. Nunc pharetra, leo eu ma nizzle hendrerizzle, dawg felizzle elementizzle sizzle, go to hizzle brizzle magna felizzle luctizzle shizzle my nizzle crocodizzle. Yippiyo a i saw beyonces tizzles and my pizzle went crizzle.",
   },
   {
     id: "employer3",
@@ -35,5 +44,37 @@ export const EMPLOYMENT = [
     startDate: "MMMM-YY",
     endDate: "MMMM-YY",
     location: "City, Country",
+    content1:
+      "Prizzle vitae erat black libero shizzlin dizzle adipiscing. Fusce ac augue eu shizznit ullamcorpizzle sure. Shiznit fermentizzle sapizzle ass erat. Suspendisse lorizzle leo, sollicitudizzle dang, mattizzle ass, commodo pimpin', justo. Break yo neck, yall boom shackalack porttitor ligula. Check it out for sure, doggy a ornare da bomb, sapizzle metizzle ante, egizzle boofron my shizz enim fo shizzle lorizzle. Phasellus quam shiznit, imperdizzle hizzle, tempizzle shut the shizzle up, sempizzle in, sapien.",
+    content2:
+      "Yo eu bling bling. Break it down porta its fo rizzle tellus. Gizzle viverra, doggy izzle away hendrerizzle, libero urna sheezy leo, fizzle doggy my shizz sapizzle cool daahng dawg. Donizzle sheezy dolizzle. Vestibulizzle shit felizzle. ",
+    content3:
+      "Tellivizzle elementum mammasay mammasa mamma oo sa nizzle. Nizzle nulla gangster, shiz izzle, volutpizzle sheezy, auctor eget, things. Sed pharetra. Mah nizzle nisi. Pot amizzle leo fo shizzle my nizzle shizznit blandit dignissim. Quisque laorizzle dizzle sit enim. Aenizzle tempizzle dignissim felizzle.",
+  },
+  {
+    id: "employer4",
+    displayName: "Employer 4",
+    url: "https://employer4.com",
+    position: "Engineer",
+    startDate: "MMMM-YY",
+    endDate: "MMMM-YY",
+    location: "City, Country",
+    content1:
+      "Crizzle break it down justo i saw beyonces tizzles and my pizzle went crizzle purizzle break it down ornare. Dawg venenatis justo izzle daahng dawg. Shizznit fizzle. Suspendisse fizzle placerat hizzle. Dang own yo' ante. Nunc pharetra, leo eu ma nizzle hendrerizzle, dawg felizzle elementizzle sizzle, go to hizzle brizzle magna felizzle luctizzle shizzle my nizzle crocodizzle. Yippiyo a i saw beyonces tizzles and my pizzle went crizzle.",
+  },
+  {
+    id: "employer5",
+    displayName: "Employer 5",
+    url: "https://employer5.com",
+    position: "Engineer",
+    startDate: "MMMM-YY",
+    endDate: "MMMM-YY",
+    location: "City, Country",
+    content1:
+      "Prizzle vitae erat black libero shizzlin dizzle adipiscing. Fusce ac augue eu shizznit ullamcorpizzle sure. Shiznit fermentizzle sapizzle ass erat. Suspendisse lorizzle leo, sollicitudizzle dang, mattizzle ass, commodo pimpin', justo. Break yo neck, yall boom shackalack porttitor ligula. Check it out for sure, doggy a ornare da bomb, sapizzle metizzle ante, egizzle boofron my shizz enim fo shizzle lorizzle. Phasellus quam shiznit, imperdizzle hizzle, tempizzle shut the shizzle up, sempizzle in, sapien.",
+    content2:
+      "Yo eu bling bling. Break it down porta its fo rizzle tellus. Gizzle viverra, doggy izzle away hendrerizzle, libero urna sheezy leo, fizzle doggy my shizz sapizzle cool daahng dawg. Donizzle sheezy dolizzle. Vestibulizzle shit felizzle. ",
+    content3:
+      "Tellivizzle elementum mammasay mammasa mamma oo sa nizzle. Nizzle nulla gangster, shiz izzle, volutpizzle sheezy, auctor eget, things. Sed pharetra. Mah nizzle nisi. Pot amizzle leo fo shizzle my nizzle shizznit blandit dignissim. Quisque laorizzle dizzle sit enim. Aenizzle tempizzle dignissim felizzle.",
   },
 ];
