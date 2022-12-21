98711a28a98b3836229354d6c966c48f07733198
Andrew James <contact@ajames.dev>
2022-12-21 09:34:39 +0000

    added remotePatterns for safe external images

--- next.config.js
+++ next.config.js
@@ -1,4 +1,12 @@
 module.exports = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "s3.us-west-2.amazonaws.com",
+      },
+    ],
+  },
   webpack(config) {
     config.module.rules.push({
       test: /\.svg$/i,
