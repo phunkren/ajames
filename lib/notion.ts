0ab6d574903fa3abaa687347406d14aa9a49973f
Andrew James <contact@ajames.dev>
2022-12-22 09:24:52 +0000

    added notion to md third-party helper

--- lib/notion.ts
+++ lib/notion.ts
@@ -1,27 +1,28 @@
+import * as fs from "fs";
 import { Client } from "@notionhq/client";
+import { NotionToMarkdown } from "notion-to-md";
+import { POSTS_DIR } from "../constants/posts";
 
 const notion = new Client({
   auth: process.env.NOTION_TOKEN,
 });
 
+// Ref: https://www.npmjs.com/package/notion-to-md
+const n2m = new NotionToMarkdown({ notionClient: notion });
+
+export const createPost = async (uuid: string, slug: string) => {
+  const mdblocks = await n2m.pageToMarkdown(uuid);
+  const mdString = n2m.toMarkdownString(mdblocks);
+  const filename = `${POSTS_DIR}/${slug}.md`;
+
+  fs.writeFile(filename, mdString, (err) => {
+    console.log(err);
+  });
+};
+
 export const getDatabase = async (databaseId: string) => {
   const response = await notion.databases.query({
     database_id: databaseId,
-  });
-
-  return response.results;
-};
-
-export const getPage = async (pageId: string) => {
-  const response = await notion.pages.retrieve({ page_id: pageId });
-
-  return response;
-};
-
-export const getBlocks = async (blockId: string) => {
-  const response = await notion.blocks.children.list({
-    block_id: blockId,
-    page_size: 50,
   });
 
   return response.results;
@@ -41,3 +42,17 @@ export const getPosts = async () => {
     professional: professionalPosts,
   };
 };
+
+// Generate a markdown file for each blog post in Notion
+(async () => {
+  const { personal, professional } = await getPosts();
+  const posts = [...personal, ...professional];
+
+  for (const post of posts) {
+    const uuid = post.id;
+    //@ts-expect-error
+    const slug = post.properties.slug.rich_text[0].plain_text;
+
+    createPost(uuid, slug);
+  }
+})();
