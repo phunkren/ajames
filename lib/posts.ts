6d341a1e06703aca0821171d64d255be3f9a2915
Andrew James <contact@ajames.dev>
2022-12-23 09:21:03 +0000

    processed the markdown string as html

--- lib/posts.ts
+++ lib/posts.ts
@@ -1,5 +1,7 @@
 import * as fs from "fs";
 import path from "path";
+import { remark } from "remark";
+import html from "remark-html";
 import { POSTS_DIR } from "../constants/posts";
 
 export function getAllPostIds() {
@@ -28,10 +30,15 @@ export function getAllPostIds() {
   });
 }
 
-export function getPostData(id) {
+export async function getPostData(id) {
   const fullPath = path.join(POSTS_DIR, `${id}.md`);
   const fileContents = fs.readFileSync(fullPath, "utf8");
 
+  // Use remark to convert markdown into HTML string
+  const processedContent = await remark().use(html).process(fileContents);
+
+  const contentHtml = processedContent.toString();
+
   // Combine the data with the id
-  return fileContents;
+  return contentHtml;
 }
