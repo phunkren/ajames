4c2d128508ff757b08460a1b9bfaca4094bd4287
Andrew James <contact@ajames.dev>
2022-12-23 09:21:28 +0000

    rednered frontmatter and blog post content on id page

--- pages/blog/[id].tsx
+++ pages/blog/[id].tsx
@@ -1,7 +1,20 @@
-import { Layout } from "../../components/Layout";
+import { GetPageResponse } from "@notionhq/client/build/src/api-endpoints";
+import Image from "next/image";
+import { HStack, Layout, VStack } from "../../components/Layout";
+import { TextAux, TextTitle1 } from "../../components/Text";
+import { getPageData } from "../../lib/notion";
 import { getAllPostIds, getPostData } from "../../lib/posts";
+import { Tag } from "../../types/notion";
+
+type Frontmatter = {
+  title: string;
+  image: string;
+  date: string;
+  tags?: Tag[];
+};
 
 type Props = {
+  frontmatter: Frontmatter;
   postData: Record<string, unknown>;
 };
 
@@ -15,19 +28,55 @@ export async function getStaticPaths() {
 }
 
 export async function getStaticProps({ params }) {
-  const postData = getPostData(params.id);
+  const pageData = await getPageData(params.id);
+  const postData = await getPostData(params.id);
+
+  const frontmatter = {
+    title: pageData.properties.page.title[0].plain_text,
+    image: pageData.cover?.file.url,
+    date: pageData.properties.date.created_time,
+    tags: pageData.properties.tags.multi_select,
+  };
 
   return {
     props: {
+      frontmatter,
       postData,
     },
   };
 }
 
-export default function BlogPost({ postData }: Props) {
+export default function BlogPost({ frontmatter, postData }: Props) {
   return (
     <Layout>
-      <div>{JSON.stringify(postData)}</div>
+      <VStack id="frontmatter" alignItems="center">
+        <Image
+          src={frontmatter.image}
+          alt="cover image"
+          width={100}
+          height={100}
+        />
+
+        <TextTitle1>{frontmatter.title}</TextTitle1>
+
+        <TextAux>{frontmatter.date}</TextAux>
+
+        {frontmatter.tags?.length > 0 ? (
+          <HStack>
+            {frontmatter.tags.map((tag) => (
+              <TextAux key={tag.id}>{tag.name}</TextAux>
+            ))}
+          </HStack>
+        ) : null}
+      </VStack>
+
+      <VStack
+        id="content"
+        as="article"
+        css={{ maxWidth: 720, margin: "0 auto" }}
+        gap={{ "@initial": 3, "@bp2": 5 }}
+        dangerouslySetInnerHTML={{ __html: postData }}
+      />
     </Layout>
   );
 }
