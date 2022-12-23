dde39300fc632f845b70989048d29c36d0ce3aac
Andrew James <contact@ajames.dev>
2022-12-23 09:20:29 +0000

    created getPageData function

--- lib/notion.ts
+++ lib/notion.ts
@@ -2,6 +2,7 @@ import * as fs from "fs";
 import { Client } from "@notionhq/client";
 import { NotionToMarkdown } from "notion-to-md";
 import { POSTS_DIR } from "../constants/posts";
+import { BlogPost } from "../types/notion";
 
 const notion = new Client({
   auth: process.env.NOTION_TOKEN,
@@ -10,13 +11,14 @@ const notion = new Client({
 // Ref: https://www.npmjs.com/package/notion-to-md
 const n2m = new NotionToMarkdown({ notionClient: notion });
 
+// Create markdown file from Notion blog post
 export const createPost = async (uuid: string, slug: string) => {
   const mdblocks = await n2m.pageToMarkdown(uuid);
   const mdString = n2m.toMarkdownString(mdblocks);
   const filename = `${POSTS_DIR}/${slug}.md`;
 
   fs.writeFile(filename, mdString, (err) => {
-    console.log(err);
+    err !== null && console.log(err);
   });
 };
 
@@ -29,18 +31,30 @@ export const getDatabase = async (databaseId: string) => {
 };
 
 export const getPosts = async () => {
-  const personalPosts = await getDatabase(
+  const personalPosts = (await getDatabase(
     process.env.NOTION_PERSONAL_DATABASE_ID
-  );
+  )) as BlogPost[];
 
-  const professionalPosts = await getDatabase(
+  const professionalPosts = (await getDatabase(
     process.env.NOTION_PROFESSIONAL_DATABASE_ID
-  );
+  )) as BlogPost[];
 
   return {
     personal: personalPosts,
     professional: professionalPosts,
   };
+};
+
+export const getPageData = async (slug: string) => {
+  const { personal, professional } = await getPosts();
+  const posts = [...personal, ...professional];
+
+  const page = posts.find((post) => {
+    const postSlug = post.properties.slug.rich_text[0].plain_text;
+    return postSlug === slug;
+  });
+
+  return page;
 };
 
 // Generate a markdown file for each blog post in Notion
@@ -50,7 +64,6 @@ export const getPosts = async () => {
 
   for (const post of posts) {
     const uuid = post.id;
-    //@ts-expect-error
     const slug = post.properties.slug.rich_text[0].plain_text;
 
     createPost(uuid, slug);
