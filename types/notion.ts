5dd0d8eeea905aca7b66305ec363217a5890d9c8
Andrew James <contact@ajames.dev>
2022-12-23 09:21:13 +0000

    Added typings for blog posts

--- types/notion.ts
+++ types/notion.ts
@@ -1,6 +1,102 @@
-import {
-  PageObjectResponse,
-  PartialPageObjectResponse,
-} from "@notionhq/client/build/src/api-endpoints";
+import { PageObjectResponse } from "@notionhq/client/build/src/api-endpoints";
 
-export type BlogPost = PageObjectResponse | PartialPageObjectResponse;
+type Authors = {
+  id: string;
+  type: string;
+  people: {
+    object: string;
+    id: string;
+    name: string;
+    avatar_url: string;
+    type: string;
+    person: {
+      email: string;
+    };
+  }[];
+};
+
+type Date = {
+  id: string;
+  type: string;
+  created_time: string;
+};
+
+type Page = {
+  id: string;
+  type: string;
+  title: {
+    type: string;
+    text: {
+      content: string;
+      link?: string;
+    };
+    annotations: {
+      bold: boolean;
+      italic: boolean;
+      strikethrough: boolean;
+      underline: boolean;
+      code: boolean;
+      color: string;
+    };
+    plain_text: string;
+    href?: string;
+  }[];
+};
+
+type Published = {
+  id: string;
+  type: string;
+  checkbox: boolean;
+};
+
+type Slug = {
+  id: string;
+  type: string;
+  rich_text: {
+    type: string;
+    text: {
+      content: string;
+      link?: string;
+    };
+    annotations: {
+      bold: boolean;
+      italic: boolean;
+      strikethrough: boolean;
+      underline: boolean;
+      code: boolean;
+      color: string;
+    };
+    plain_text: string;
+    href?: string;
+  }[];
+};
+
+export type Tag = {
+  id: string;
+  name: string;
+  color: string;
+};
+
+type Tags = {
+  id: string;
+  type: string;
+  multi_select: Tag[];
+};
+
+export type BlogPost = PageObjectResponse & {
+  cover: {
+    type: "file";
+    file: {
+      url: string;
+      expiry_time: string;
+    };
+  };
+  properties: {
+    authors: Authors;
+    date: Date;
+    page: Page;
+    published: Published;
+    slug: Slug;
+    tags: Tags;
+  };
+};
