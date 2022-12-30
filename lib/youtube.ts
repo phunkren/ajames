18080905b777074c90a89e69b7002a7f2f2be7d0
Andrew James <contact@ajames.dev>
2022-12-30 16:52:07 +0000

    add format helper functions to youtube lib

--- lib/youtube.ts
+++ lib/youtube.ts
@@ -1,8 +1,15 @@
 import { buildUrl } from "../helpers/url";
+import { PlaylistPreview, VideoPreview } from "../types/youtube";
+
+const YOUTUBE_URL = "https://www.youtube.com/";
 
 const YOUTUBE_API_URL = "https://youtube.googleapis.com/youtube/v3";
 
 const YOUTUBE_CHANNEL_ID = "UCCp_G-IprVFee-tBabTROsw";
+
+export const getSubscriptionUrl = () => {
+  return buildUrl(`${YOUTUBE_URL}/@ajamesdev`, { sub_confirmation: 1 });
+};
 
 export const getLatestVideo = async () => {
   const latestVideoUrl = buildUrl(`${YOUTUBE_API_URL}/search`, {
@@ -53,4 +60,49 @@ export const getPlaylists = async () => {
   const data = await response.json();
 
   return data;
+};
+
+export const formatLatestResponse = (response: any): VideoPreview => {
+  const latest = response.items[0];
+  const { thumbnails, title, publishedAt, description } = latest.snippet;
+  const url = buildUrl(`${YOUTUBE_URL}/watch`, { v: latest.id });
+
+  const thumbnail = {
+    src: thumbnails?.high.url,
+    width: thumbnails?.high.width,
+    height: thumbnails?.high.height,
+    alt: title,
+  };
+
+  return {
+    thumbnail,
+    title,
+    publishedAt,
+    description,
+    url,
+  };
+};
+
+export const formatPlaylistsResponse = (response: any): PlaylistPreview[] => {
+  const playlists = response.items.map((item) => {
+    const { thumbnails, title, description } = item.snippet;
+    const url = buildUrl(`${YOUTUBE_URL}/playlist`, { list: item.id });
+
+    const thumbnail = {
+      src: thumbnails?.high.url,
+      width: thumbnails?.high.width,
+      height: thumbnails?.high.height,
+      alt: title,
+    };
+
+    return {
+      id: item.id,
+      title,
+      description,
+      thumbnail,
+      url,
+    };
+  });
+
+  return playlists;
 };
