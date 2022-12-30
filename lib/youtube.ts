758f09de0ef415ba07030bd78da8a3165f64daa2
Andrew James <contact@ajames.dev>
2022-12-30 15:22:31 +0000

    refactor youtube helpers to use build url helper

--- lib/youtube.ts
+++ lib/youtube.ts
@@ -1,11 +1,21 @@
+import { buildUrl } from "../helpers/url";
+
 const YOUTUBE_API_URL = "https://youtube.googleapis.com/youtube/v3";
 
 const YOUTUBE_CHANNEL_ID = "UCCp_G-IprVFee-tBabTROsw";
 
 export const getLatestVideo = async () => {
-  const response = await fetch(
-    `${YOUTUBE_API_URL}/search?part=snippet&channelId=${YOUTUBE_CHANNEL_ID}&eventType=completed&maxResults=1&order=date&type=video&key=${process.env.YOUTUBE_TOKEN}`
-  );
+  const latestVideoUrl = buildUrl(`${YOUTUBE_API_URL}/search`, {
+    part: "snippet",
+    channelId: YOUTUBE_CHANNEL_ID,
+    eventType: "completed",
+    maxResults: 1,
+    order: "date",
+    type: "video",
+    key: process.env.YOUTUBE_TOKEN,
+  });
+
+  const response = await fetch(latestVideoUrl);
 
   const data = await response.json();
 
@@ -13,9 +23,17 @@ export const getLatestVideo = async () => {
 };
 
 export const getLatestLiveStream = async () => {
-  const response = await fetch(
-    `${YOUTUBE_API_URL}/search?part=snippet&channelId=${YOUTUBE_CHANNEL_ID}&eventType=live&maxResults=1&order=date&type=video&key=${process.env.YOUTUBE_TOKEN}`
-  );
+  const latestLiveStreamUrl = buildUrl(`${YOUTUBE_API_URL}/search`, {
+    part: "snippet",
+    channelId: YOUTUBE_CHANNEL_ID,
+    eventType: "live",
+    maxResults: 1,
+    order: "date",
+    type: "video",
+    key: process.env.YOUTUBE_TOKEN,
+  });
+
+  const response = await fetch(latestLiveStreamUrl);
 
   const data = await response.json();
 
@@ -23,9 +41,14 @@ export const getLatestLiveStream = async () => {
 };
 
 export const getPlaylists = async () => {
-  const response = await fetch(
-    `${YOUTUBE_API_URL}/playlists?part=snippet&channelId=${YOUTUBE_CHANNEL_ID}&maxResults=5&key=${process.env.YOUTUBE_TOKEN}`
-  );
+  const playlistsUrl = buildUrl(`${YOUTUBE_API_URL}/playlists`, {
+    part: "snippet",
+    channelId: YOUTUBE_CHANNEL_ID,
+    maxResults: 5,
+    key: process.env.YOUTUBE_TOKEN,
+  });
+
+  const response = await fetch(playlistsUrl);
 
   const data = await response.json();
 
