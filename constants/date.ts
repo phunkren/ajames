c05cde1b7c357bcc6f6242e32637b8b4f59d1538
Andrew James <contact@ajames.dev>
2023-01-03 22:13:30 +0000

    add correct time to hour in seconds

--- constants/date.ts
+++ constants/date.ts
@@ -1,4 +1,1 @@
-// This is actually one second shy of one hour.
-// The reason is that Notion images have an expiry time of one hour.
-// We revalidate the ISR cache just before the images can expire.
-export const ONE_HOUR_IN_SECONDS = 3599;
+export const ONE_HOUR_IN_SECONDS = 3600;
