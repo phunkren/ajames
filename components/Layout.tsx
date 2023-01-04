f13e3978c47582bd5fa577193610c52dc55d2947
Andrew James <contact@ajames.dev>
2023-01-04 12:07:32 +0000

    updated blog post layout

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -1,3 +1,5 @@
+import Image from "next/image";
+import * as AspectRatio from "@radix-ui/react-aspect-ratio";
 import { useTheme } from "../hooks/useTheme";
 import { styled } from "../stitches.config";
 import { DISPLAY_VARIANTS } from "../styles/display";
@@ -8,12 +10,32 @@ import { Link } from "./Link";
 import { Navigation, NavigationMobile } from "./Navigation";
 import { Social } from "./Social";
 import { ThemeToggle } from "./Theme";
+import { Button, ScrollToTopButton } from "./Button";
 
 const StyledBox = styled("div", {
   display: "flex",
   flexDirection: "row",
 
   variants: { ...FLEX_VARIANTS, ...SPACING_VARIANTS, ...DISPLAY_VARIANTS },
+});
+
+const StyledImage = styled(Image, {
+  objectFit: "cover",
+});
+
+// These magic numbers are calculated based on the font size of the TextTitle1 component
+// The computed value is (fontSize * lineHeight) / 2.
+const StyledContent = styled(Box, {
+  position: "relative",
+  top: -27,
+
+  "@bp2": {
+    top: -36,
+  },
+
+  "@bp3": {
+    top: -54,
+  },
 });
 
 export function RootLayout({ children }) {
@@ -24,9 +46,41 @@ export function RootLayout({ children }) {
       id="__root"
       direction="vertical"
       className={theme}
-      css={{ maxWidth: "1400px", margin: "0 auto" }}
+      css={{ maxWidth: "1200px", margin: "0 auto" }}
     >
       {children}
+    </Box>
+  );
+}
+
+export function HeaderLayout() {
+  return (
+    <Box
+      as="header"
+      direction="horizontal"
+      spacingVertical={{ "@initial": 4, "@bp2": 7 }}
+      spacingHorizontal={2}
+      gap={{ "@initial": 4, "@bp2": 7 }}
+      justifyContent="space-between"
+      alignItems="center"
+    >
+      <Box direction="horizontal" gap={7} alignItems="center">
+        <Link href="/">
+          <Avatar />
+        </Link>
+
+        <Box display={{ "@initial": "none", "@bp2": "flex" }}>
+          <Navigation />
+        </Box>
+      </Box>
+
+      <Box display={{ "@initial": "none", "@bp2": "flex" }}>
+        <ThemeToggle />
+      </Box>
+
+      <Box display={{ "@initial": "flex", "@bp2": "none" }}>
+        <NavigationMobile />
+      </Box>
     </Box>
   );
 }
@@ -38,33 +92,7 @@ export function Layout({ children }) {
       spacingHorizontal={{ "@initial": 2, "@bp2": 4 }}
       flexGrow
     >
-      <Box
-        as="header"
-        direction="horizontal"
-        spacingVertical={{ "@initial": 4, "@bp2": 7 }}
-        spacingHorizontal={2}
-        gap={{ "@initial": 4, "@bp2": 7 }}
-        justifyContent="space-between"
-        alignItems="center"
-      >
-        <Box direction="horizontal" gap={7} alignItems="center">
-          <Link href="/">
-            <Avatar />
-          </Link>
-
-          <Box display={{ "@initial": "none", "@bp2": "flex" }}>
-            <Navigation />
-          </Box>
-        </Box>
-
-        <Box display={{ "@initial": "none", "@bp2": "flex" }}>
-          <ThemeToggle />
-        </Box>
-
-        <Box display={{ "@initial": "flex", "@bp2": "none" }}>
-          <NavigationMobile />
-        </Box>
-      </Box>
+      <HeaderLayout />
 
       <Box as="main" direction="vertical" flexGrow>
         {children}
@@ -78,6 +106,36 @@ export function Layout({ children }) {
       >
         <Social />
       </Box>
+    </Box>
+  );
+}
+
+export function BlogLayout({ hero, children }) {
+  return (
+    <Box direction="vertical" spacingHorizontal={{ "@initial": 2, "@bp2": 4 }}>
+      <Box direction="vertical">
+        <HeaderLayout />
+
+        <AspectRatio.Root ratio={16 / 9}>
+          <StyledImage src={hero} alt="" fill />
+        </AspectRatio.Root>
+      </Box>
+
+      <StyledContent as="main" direction="vertical" flexGrow>
+        {children}
+      </StyledContent>
+
+      <Box
+        as="footer"
+        direction="horizontal"
+        spacingVertical={{ "@initial": 4, "@bp2": 7 }}
+        justifyContent="center"
+      >
+        {/* social sharing (social-media-specific, or copy to clipboard button) */}
+        <Social />
+      </Box>
+
+      <ScrollToTopButton />
     </Box>
   );
 }
--- pages/blog/[id].tsx
+++ pages/blog/[id].tsx
@@ -1,4 +1,3 @@
-import Image from "next/image";
 import remarkMdx from "remark-mdx";
 import ReactMarkdown from "react-markdown";
 import Balancer from "react-wrap-balancer";
@@ -6,14 +5,17 @@ import { Tag } from "../../types/notion";
 import { getPageData } from "../../lib/notion";
 import { getAllPostIds, getPostData } from "../../lib/posts";
 import { ONE_HOUR_IN_SECONDS } from "../../constants/date";
-import { Box, Layout } from "../../components/Layout";
+import { BlogLayout, Box, Layout } from "../../components/Layout";
 import { MarkdownLink } from "../../components/Link";
 import { TextAux, TextTitle1 } from "../../components/Text";
 import { Code } from "../../components/Code";
+import { CalendarIcon, ClockIcon, ListBulletIcon } from "@radix-ui/react-icons";
+import { Divider } from "../../components/Divider";
 
 type Frontmatter = {
   title: string;
   cover: string;
+  emoji: string;
   date: string;
   tags: Tag[];
 };
@@ -36,9 +38,10 @@ export async function getStaticProps({ params }) {
   const pageData = await getPageData(params.id);
   const postData = await getPostData(params.id);
 
-  const frontmatter = {
+  const frontmatter: Frontmatter = {
     title: pageData.properties.page.title[0].plain_text,
     cover: pageData.cover.external.url,
+    emoji: pageData.icon.type === "emoji" ? pageData.icon.emoji : "👨‍💻",
     date: new Date(pageData.properties.date.date.start).toISOString(),
     tags: pageData.properties.tags.multi_select,
   };
@@ -47,6 +50,7 @@ export async function getStaticProps({ params }) {
     props: {
       frontmatter,
       postData,
+      pageData,
     },
     revalidate: ONE_HOUR_IN_SECONDS,
   };
@@ -54,38 +58,64 @@ export async function getStaticProps({ params }) {
 
 export default function BlogPost({ frontmatter, postData }: Props) {
   return (
-    <Layout>
-      <Box direction="vertical" alignItems="center">
-        <Image src={frontmatter.cover} alt="" width={100} height={100} />
+    <BlogLayout hero={frontmatter.cover}>
+      <Box as="article" direction="vertical">
+        <Box spacingHorizontal={{ "@bp3": 7 }}>
+          <TextTitle1>
+            <span aria-hidden style={{ display: "block" }}>
+              {frontmatter.emoji}
+            </span>
 
-        <TextTitle1>
-          <Balancer>{frontmatter.title}</Balancer>
-        </TextTitle1>
+            <Balancer>{frontmatter.title}</Balancer>
+          </TextTitle1>
+        </Box>
 
-        <TextAux>{frontmatter.date}</TextAux>
+        <Box
+          direction="vertical"
+          css={{ maxWidth: 720, margin: "0 auto" }}
+          gap={{ "@initial": 4, "@bp2": 7 }}
+        >
+          <Box
+            id="frontmatter"
+            as="ul"
+            direction="vertical"
+            gap={4}
+            spacingTop={7}
+            css={{ maxWidth: 720, margin: "0 auto" }}
+          >
+            <Box as="li" alignItems="center" gap={4}>
+              <ListBulletIcon width={28} height={28} />
+              {frontmatter.tags.map((tag) => (
+                <TextAux key={tag.id}>{tag.name}</TextAux>
+              ))}
+            </Box>
 
-        <Box direction="horizontal">
-          {frontmatter.tags.map((tag) => (
-            <TextAux key={tag.id}>{tag.name}</TextAux>
-          ))}
+            <Box as="li" alignItems="center" gap={4}>
+              <CalendarIcon width={28} height={28} />
+              <TextAux>{frontmatter.date}</TextAux>
+            </Box>
+
+            <Box as="li" alignItems="center" gap={4}>
+              <ClockIcon width={28} height={28} />
+              {/* TODO: Add calculated reading time */}
+              <TextAux>10 minutes</TextAux>
+            </Box>
+          </Box>
+
+          <Box spacingVertical={7}>
+            <Divider />
+          </Box>
+
+          <ReactMarkdown
+            children={postData}
+            components={{
+              a: MarkdownLink,
+              code: Code,
+            }}
+            remarkPlugins={[remarkMdx]}
+          />
         </Box>
       </Box>
-
-      <Box
-        direction="vertical"
-        as="article"
-        css={{ maxWidth: 720, margin: "0 auto" }}
-        gap={{ "@initial": 3, "@bp2": 5 }}
-      >
-        <ReactMarkdown
-          children={postData}
-          components={{
-            a: MarkdownLink,
-            code: Code,
-          }}
-          remarkPlugins={[remarkMdx]}
-        />
-      </Box>
-    </Layout>
+    </BlogLayout>
   );
 }
