9918a071e57e2ded0b3d7a419b396eb6ec4f77e1
Andrew James <contact@ajames.dev>
2023-01-07 22:02:35 +0000

    add css type to link props

--- types/link.ts
+++ types/link.ts
@@ -1,12 +1,11 @@
 import { AnchorHTMLAttributes } from "react";
 import { LinkProps as NextLinkProps } from "next/link";
 import { UrlObject } from "url";
+import { CSS } from "../stitches.config";
 
-export type LinkProps = Omit<
-  AnchorHTMLAttributes<HTMLAnchorElement>,
-  "href"
-> & {
-  href: string | UrlObject;
-  nextLinkProps?: Omit<NextLinkProps, "href">;
-  variant?: "primary" | "secondary" | "tertiary";
-};
+export type LinkProps = CSS &
+  Omit<AnchorHTMLAttributes<HTMLAnchorElement>, "href"> & {
+    href: string | UrlObject;
+    nextLinkProps?: Omit<NextLinkProps, "href">;
+    variant?: "primary" | "secondary" | "tertiary";
+  };
