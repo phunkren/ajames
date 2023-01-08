335e3202bcfeca57305a9ce0710e02a2eaef9d1e
Andrew James <contact@ajames.dev>
2023-01-08 12:13:51 +0000

    add date formatting functions

--- util/date.ts
+++ util/date.ts
@@ -1,1 +1,15 @@
+import format from "date-fns/format";
+
 export const ONE_HOUR_IN_SECONDS = 3600;
+
+export function formatLongDate(date: Date) {
+  const formattedDate = format(date, "MMMM do, yyyy");
+
+  return formattedDate;
+}
+
+export function formatShortDate(date: Date) {
+  const formattedDate = format(date, "dd/MM/yyyy");
+
+  return formattedDate;
+}
