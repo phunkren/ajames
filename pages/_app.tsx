6e941dbd0dedc682b48822cc769a196fac845242
Andrew James <contact@ajames.dev>
2023-01-12 08:31:07 +0000

    update error boundary

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,16 +3,26 @@ import { RootLayout } from "../components/Layout";
 import { ThemeProvider } from "../components/Theme";
 import { globalStyles } from "../styles/global";
 
+function ErrorFallback({ error, resetErrorBoundary }) {
+  return (
+    <div role="alert">
+      <p>Something went wrong:</p>
+      <pre>{error.message}</pre>
+      <button onClick={resetErrorBoundary}>Try again</button>
+    </div>
+  );
+}
+
 export default function MyApp({ Component, pageProps }) {
   globalStyles();
 
   return (
-    <ThemeProvider>
-      <ErrorBoundary fallback={<div>Oh no</div>}>
+    <ErrorBoundary FallbackComponent={ErrorFallback}>
+      <ThemeProvider>
         <RootLayout>
           <Component {...pageProps} />
         </RootLayout>
-      </ErrorBoundary>
-    </ThemeProvider>
+      </ThemeProvider>
+    </ErrorBoundary>
   );
 }
