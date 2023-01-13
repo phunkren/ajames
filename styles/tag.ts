c561a7b3466fbbdaf6fa1f655352aa6236ef397e
Andrew James <contact@ajames.dev>
2023-01-13 07:21:10 +0000

    refactor tag styles

--- styles/tag.ts
+++ styles/tag.ts
@@ -34,94 +34,34 @@ export const NOTION_TAG_VARIANTS = {
   },
   borderColor: {
     red: {
-      [`${lightTheme.selector} &`]: {
-        borderColor: tomato.tomato9,
-      },
-
-      [`${darkTheme.selector} &`]: {
-        borderColor: tomatoDark.tomato9,
-      },
+      borderColor: "$tomato9",
     },
     orange: {
-      [`${lightTheme.selector} &`]: {
-        borderColor: amber.amber9,
-      },
-
-      [`${darkTheme.selector} &`]: {
-        borderColor: amberDark.amber9,
-      },
+      borderColor: "$amber9",
     },
     yellow: {
-      [`${lightTheme.selector} &`]: {
-        borderColor: yellow.yellow9,
-      },
-
-      [`${darkTheme.selector} &`]: {
-        borderColor: yellowDark.yellow9,
-      },
+      borderColor: "$yellow9",
     },
     green: {
-      [`${lightTheme.selector} &`]: {
-        borderColor: grass.grass9,
-      },
-
-      [`${darkTheme.selector} &`]: {
-        borderColor: grassDark.grass9,
-      },
+      borderColor: "$grass9",
     },
     blue: {
-      [`${lightTheme.selector} &`]: {
-        borderColor: sky.sky9,
-      },
-
-      [`${darkTheme.selector} &`]: {
-        borderColor: skyDark.sky9,
-      },
+      borderColor: "$sky9",
     },
     purple: {
-      [`${lightTheme.selector} &`]: {
-        borderColor: plum.plum9,
-      },
-
-      [`${darkTheme.selector} &`]: {
-        borderColor: plumDark.plum9,
-      },
+      borderColor: "$plum9",
     },
     pink: {
-      [`${lightTheme.selector} &`]: {
-        borderColor: pink.pink9,
-      },
-
-      [`${darkTheme.selector} &`]: {
-        borderColor: pinkDark.pink9,
-      },
+      borderColor: "$pink9",
     },
     gray: {
-      [`${lightTheme.selector} &`]: {
-        borderColor: slate.slate9,
-      },
-
-      [`${darkTheme.selector} &`]: {
-        borderColor: slateDark.slate9,
-      },
+      borderColor: "$slate9",
     },
     brown: {
-      [`${lightTheme.selector} &`]: {
-        borderColor: brown.brown9,
-      },
-
-      [`${darkTheme.selector} &`]: {
-        borderColor: brownDark.brown9,
-      },
+      borderColor: "$brown9",
     },
     default: {
-      [`${lightTheme.selector} &`]: {
-        borderColor: gold.gold9,
-      },
-
-      [`${darkTheme.selector} &`]: {
-        borderColor: goldDark.gold9,
-      },
+      borderColor: "$gold9",
     },
   },
 };
