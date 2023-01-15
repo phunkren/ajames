52361c80e4c3c5254d718c22c1377ae8145f2d99
Andrew James <contact@ajames.dev>
2023-01-15 01:29:04 +0000

    temp font fix

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -6,7 +6,7 @@ export default function Document() {
     <Html lang="en">
       <Head>
         <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+        <link rel="preconnect" href="https://fonts.gstatic.com" />
         <link
           href="https://fonts.googleapis.com/css2?family=Alexandria:wght@300;400;500&family=Arimo:wght@400;500&family=Chivo:wght@300;400;500&family=Finlandica:wght@400;500&family=Jost:wght@300;400;500&family=Major+Mono+Display&family=Montserrat:wght@200;300;400;500&family=Open+Sans:wght@300;400;500&family=Rubik:wght@300;400;500&family=Sora:wght@300;400;500&display=swap"
           rel="stylesheet"
