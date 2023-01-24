52a210e08e3cb10b5df05dd8cb46e20a8cabca40
Andrew James <contact@ajames.dev>
2023-01-24 23:26:22 +0000

    add fist bump animation

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,8 +5,59 @@ import { Layout } from "../components/Layout";
 import { Link } from "../components/Link";
 import { PageToggle } from "../components/Tags";
 import { Emoji, TextBody, TextHeadline, TextTitle1 } from "../components/Text";
+import { keyframes, styled } from "../stitches.config";
 import { EMPLOYMENT, PERSONAL } from "../util/data";
 import { NextPageWithLayout } from "./_app";
+
+const wave = keyframes({
+  "0%": { transform: "rotate(20deg)" },
+  "50%": { transform: "rotate(50deg)" },
+  "100%": { transform: "rotate(20deg)" },
+});
+
+const punch = keyframes({
+  "0%": { transform: "scale(1)" },
+  "50%": { transform: "scale(1.5)" },
+  "100%": { transform: "scale(1)" },
+});
+
+const StyledHand = styled(Box, {
+  position: "relative",
+  width: 44,
+  height: 44,
+
+  "&:hover": {
+    cursor: "grabbing",
+
+    "& > span[data-palm=true]": {
+      visibility: "hidden",
+    },
+
+    "& > span[data-fist=true]": {
+      display: "block",
+    },
+  },
+});
+
+const StyledPalm = styled(Emoji, {
+  position: "absolute",
+  width: "100%",
+  height: "100%",
+  visibility: "visible",
+  transform: "rotate(20deg)",
+  animation: `${wave} 400ms linear 600ms 2 forwards`,
+});
+
+const StyledFist = styled(Emoji, {
+  position: "absolute",
+  display: "none",
+  width: "100%",
+  height: "100%",
+
+  "&:hover": {
+    animation: `${punch} 400ms ease-out 600ms forwards`,
+  },
+});
 
 const Home: NextPageWithLayout = () => {
   const { pathname, push, query } = useRouter();
@@ -60,7 +111,10 @@ const Home: NextPageWithLayout = () => {
 
       {query.tab === "long" ? (
         <Box direction="vertical" gap={5} container="s">
-          <Emoji emoji="👋" size="s" />
+          <StyledHand>
+            <StyledPalm data-palm emoji="👋" size="s" />
+            <StyledFist data-fist emoji="👊" size="s" />
+          </StyledHand>
           <TextBody>{PERSONAL.profile1}</TextBody>
           <TextBody>{PERSONAL.profile2}</TextBody>
         </Box>
