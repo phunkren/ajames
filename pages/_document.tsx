bac09e1a1e79cd09d37ad2c87591b115eaad7227
Andrew James <contact@ajames.dev>
2023-04-27 19:20:25 +0100

    begone serviceworker

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,9 +1,20 @@
 import { Html, Head, Main, NextScript } from "next/document";
 import { getCssText } from "../stitches.config";
 import { globalStyles } from "../styles/global";
+import { SITE } from "../util/data";
 
 export default function Document() {
   globalStyles();
+
+  if ("serviceWorker" in navigator) {
+    navigator.serviceWorker.getRegistrations().then(function (registrations) {
+      for (let registration of registrations) {
+        if (registration.scope.startsWith(SITE.url)) {
+          registration.unregister();
+        }
+      }
+    });
+  }
 
   return (
     <Html lang="en">
