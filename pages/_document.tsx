2d5fdeef764f6d8c4b9736d2e006d4e78b28945b
Andrew James <contact@ajames.dev>
2023-04-27 19:21:56 +0100

    wrap in useEffect

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,4 +1,5 @@
 import { Html, Head, Main, NextScript } from "next/document";
+import { useEffect } from "react";
 import { getCssText } from "../stitches.config";
 import { globalStyles } from "../styles/global";
 import { SITE } from "../util/data";
@@ -6,15 +7,17 @@ import { SITE } from "../util/data";
 export default function Document() {
   globalStyles();
 
-  if ("serviceWorker" in navigator) {
-    navigator.serviceWorker.getRegistrations().then(function (registrations) {
-      for (let registration of registrations) {
-        if (registration.scope.startsWith(SITE.url)) {
-          registration.unregister();
+  useEffect(() => {
+    if ("serviceWorker" in navigator) {
+      navigator.serviceWorker.getRegistrations().then(function (registrations) {
+        for (let registration of registrations) {
+          if (registration.scope.startsWith(SITE.url)) {
+            registration.unregister();
+          }
         }
-      }
-    });
-  }
+      });
+    }
+  }, []);
 
   return (
     <Html lang="en">
